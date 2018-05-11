62bcdebd8e2096bbfa614a2ed8de24a6b115754e
zxwsbg <1036945050@qq.com>
2018-05-11 18:58:44 +0800

    0.0.3
    更新了随机化方法

--- JPGOWFunc_Calc_dist.m
+++ JPGOWFunc_Calc_dist.m
@@ -11,23 +11,35 @@ m = size(one,1);
 n = size(two,1);
 minValue=[];
 
-%% 随机化选出一些块，否则块太多
-num=0.05;  %%记录随机选取1/20的块
-num=round(num*m); %%num记录随机取的块的数量
-p=randperm(m,num);  %返回一行从1到n的整数中的num个，且这num个数也是不相同的
+%% 随机化对one矩阵选出一些块，否则块太多
+num1=0.05;  %%记录随机选取1/20的块
+num1=round(num1*m); %%num记录随机取的块的数量
+p=randperm(m,num1);  %返回一行从1到n的整数中的num个，且这num个数也是不相同的
 p=sort(p,2);
 selected_local_hist=[];
 
 %% selected_local_hist为随机选出来的部分矩阵
-for i=1:num
+for i=1:num1
     selected_local_hist=[selected_local_hist;one(p(i),:)];
 end
      
+%% 随机化对two矩阵选出一些块，否则块太多
+num2=0.05;
+num2=round(num2*n); %%num记录随机取的块的数量
+p=randperm(n,num2);  %返回一行从1到n的整数中的num个，且这num个数也是不相同的
+p=sort(p,2);
+cmp_local_hist=[];
+
+%% cmp_local_hist为随机选出来的部分矩阵
+for i=1:num2
+    cmp_local_hist=[cmp_local_hist;two(p(i),:)];
+end
+
 %% 进行论文中式子2计算
-for i=1:num
+for i=1:num1
 % temp2=abs(repmat(one(i,:),m,1)-two)./temp1;
-temp = repmat(selected_local_hist(i,:),n,1);
-temp2=abs(temp-two)./abs(1+0.000001+temp+two);
+temp = repmat(selected_local_hist(i,:),num2,1);
+temp2=abs(temp-cmp_local_hist)./abs(1+0.000001+temp+cmp_local_hist);
 minValue=[minValue;min(sum(temp2,2))];
 end
 maxminValue=sum(minValue);
--- main2_calc_dist.m
+++ main2_calc_dist.m
@@ -8,11 +8,13 @@ clear all;
 clc;
 
 %% 参数设定
-strImgFolder = 'D:\jpg1\';
+strImgFolder = 'D:\jpg\';
 retrival_type='CH';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dirOutput = dir(fullfile(strImgFolder,'*.jpg'));
+imgNames={dirOutput.name};
+imageNum = size(imgNames,2)
 localFeatureSelectStep  = 1;%% 
-imgNum = 10;
 clusterNum              = 100;%% 目前100是最好。
 maxIter                 = 300;%% 有时候可以收敛。
 clusterDistanceType     = 'cityblock';%%
@@ -27,26 +29,28 @@ strDataLoad = [strDataLoad 'LocalHistograms'];
 data = load(strDataLoad);
 
 %% 顺手算个全局标准差,虽然后来发现没有用得到,但毕竟是输入参数
-temp_Y=[];
-temp_U=[];
-temp_V=[];
-for i=1:imgNum
-    localHistograms_Y=cell2mat(data.localHistograms_Y(i));
-    localHistograms_U=cell2mat(data.localHistograms_U(i));
-    localHistograms_V=cell2mat(data.localHistograms_V(i));
-    temp_Y = [temp_Y;localHistograms_Y(:,1)];
-    temp_U = [temp_U;localHistograms_U(:,1)];
-    temp_V = [temp_V;localHistograms_V(:,1)];
-end
-AllStd_Y=std(temp_Y,0,1);
-AllStd_U=std(temp_U,0,1);
-AllStd_V=std(temp_V,0,1);
+% temp_Y=[];
+% temp_U=[];
+% temp_V=[];
+% tic;
+% for i=1:imageNum
+%     localHistograms_Y=cell2mat(data.localHistograms_Y(i));
+%     localHistograms_U=cell2mat(data.localHistograms_U(i));
+%     localHistograms_V=cell2mat(data.localHistograms_V(i));
+%     temp_Y = [temp_Y;localHistograms_Y(:,1)];
+%     temp_U = [temp_U;localHistograms_U(:,1)];
+%     temp_V = [temp_V;localHistograms_V(:,1)];
+% end
+% toc;
+ AllStd_Y=0;
+ AllStd_U=0;
+ AllStd_V=0;
  
 %% 将每一幅图都与其他图片比较，计算出他们的距离
 %%由于比较的不对称性，a和b的距离 与 b和a的距离 可能是不同的
 tic;
-for i=1:imgNum-1
-    for j=i+1:imgNum
+for i=1:imageNum-1
+    for j=i+1:imageNum
         MatrixDist_Y(i,j)=JPGOWFunc_Calc_dist(AllStd_Y,cell2mat(data.localHistograms_Y(i)),cell2mat(data.localHistograms_Y(j)));
         MatrixDist_Y(j,i)=JPGOWFunc_Calc_dist(AllStd_Y,cell2mat(data.localHistograms_Y(j)),cell2mat(data.localHistograms_Y(i)));
         MatrixDist_U(i,j)=JPGOWFunc_Calc_dist(AllStd_U,cell2mat(data.localHistograms_U(i)),cell2mat(data.localHistograms_U(j)));
@@ -65,7 +69,7 @@ w3=1;
 MatrixDist_all = MatrixDist_Y*w1+MatrixDist_U*w2+MatrixDist_V*w3;
 
 %% 存储结果
-strDataSave = 'D:\JOBGW\Encrypted-JPEG-Image-Retrival\';
+strDataSave = 'D:\JPGOW\Encrypted-JPEG-Image-Retrival\';
 strDataSave = [strDataSave retrival_type];
 strDataSave = [strDataSave 'MatrixDist'];
-save([strDataSave '.mat'],'MatrixDist_Y','MatrixDist_all ','MatrixDist_U','MatrixDist_V','runningTime', '-v7.3');
+save([strDataSave '.mat'],'MatrixDist_Y','MatrixDist_all','MatrixDist_U','MatrixDist_V','runningTime', '-v7.3');
